e156ff09f03fb3a353f303044c061640b14317dd
Jari <personal@jariketting.com>
2020-06-03 13:33:00 +0200

    Add doc

--- DatabaseTests.cs
+++ DatabaseTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using meldboek;
 using meldboek.Models;
 using Neo4j.Driver;
@@ -18,62 +17,78 @@ namespace XUnitTestMeldboek
     {
         Database Db { get; set; }
 
-        private int randomId;
-        private string firstName;
-        private string lastName;
-        private string email;
-        private string passwd;
+        private int randomId; // random id of user
+        private string firstName; // first name
+        private string lastName;  // last name
+        private string email; // email
+        private string passwd; // password
 
+        /// <summary>
+        /// Init test with database connection and generate random names
+        /// </summary>
         public DatabaseTests()
         {
-            randomId = Faker.NumberFaker.Number();
-            firstName = Faker.NameFaker.FirstName();
-            lastName = Faker.NameFaker.LastName();
-            email = firstName + lastName + "@test.com";
-            passwd = firstName + lastName + "!";
+            randomId = Faker.NumberFaker.Number(); // create random id for user
+            firstName = Faker.NameFaker.FirstName(); // create random fist name
+            lastName = Faker.NameFaker.LastName(); // create random last name
+            email = firstName + lastName + "@test.com"; // build email with first and last name
+            passwd = firstName + lastName + "!"; // build password with first and last name
 
-            Db = new Database();
-            AddTestPerson();
+            Db = new Database(); // init database
+            AddTestPerson(); // add test person
         }
 
+        /// <summary>
+        /// Add test person to databae with random generated naming
+        /// </summary>
         private void AddTestPerson()
         {
+            // run query
             _ = Db.ConnectDb("CREATE (p:Person { PersonId: " + randomId + ", FirstName: '" + firstName + "', LastName: '" + lastName+ "', Email: '" + email + "', Password: '" + passwd + "' })");
-            System.Threading.Thread.Sleep(500);
+            System.Threading.Thread.Sleep(500); // prevent some issues
         }
 
+        /// <summary>
+        /// Get person from database and check values
+        /// </summary>
         [Fact]
         public void GetValueDatabase_correctQuery()
         {
-            var getNodes = Db.ConnectDb("MATCH (p:Person) WHERE p.PersonId = " + randomId + " RETURN p LIMIT 1");
-            var user = new Person();
+            var getNodes = Db.ConnectDb("MATCH (p:Person) WHERE p.PersonId = " + randomId + " RETURN p LIMIT 1"); // get person node
+            var user = new Person(); // create new user
 
-            List<INode> nodes = getNodes.Result;
+            List<INode> nodes = getNodes.Result; // list with nodes
             foreach (var record in nodes)
             {
-                var nodeprops = JsonConvert.SerializeObject(record.As<INode>().Properties);
+                // extract user from node
+                var nodeprops = JsonConvert.SerializeObject(record.As<INode>().Properties); 
                 user = (JsonConvert.DeserializeObject<Person>(nodeprops));
             }
 
-            Assert.Equal(firstName, user.FirstName);
-            Assert.Equal(lastName, user.LastName);
-            Assert.Equal(email, user.Email);
-            Assert.Equal(passwd, user.Password);
+            Assert.Equal(firstName, user.FirstName); // check if fist name is correct
+            Assert.Equal(lastName, user.LastName); // check if last name is correct
+            Assert.Equal(email, user.Email); // check if email is correct
+            Assert.Equal(passwd, user.Password); // check if password is correct
         }
 
+        /// <summary>
+        /// Wrong database query, should return null values
+        /// </summary>
         [Fact]
         public void GetValueDatabase_wrongQuery()
         {
-            var getNodes = Db.ConnectDb("MATCH (p:Persons) WHERE p.PersonId = " + randomId + " RETURN p LIMIT 1");
+            var getNodes = Db.ConnectDb("MATCH (p:Persons) WHERE p.PersonId = " + randomId + " RETURN p LIMIT 1"); // wrong query
             var user = new Person();
 
-            List<INode> nodes = getNodes.Result;
+            List<INode> nodes = getNodes.Result; // list with nodes
             foreach (var record in nodes)
             {
+                // extact potential user
                 var nodeprops = JsonConvert.SerializeObject(record.As<INode>().Properties);
                 user = (JsonConvert.DeserializeObject<Person>(nodeprops));
             }
 
+            // check if all values are null (as they should)
             Assert.Null(user.FirstName);
             Assert.Null(user.LastName);
             Assert.Null(user.Email);
